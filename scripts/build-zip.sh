70aa024bca613a379eb85a49ef3ca5283560a6ed
Sharoon <sharoon7171@gmail.com>
2026-03-29 17:13:18 +0500

    remove all prior versioned extension zips in build before packaging

--- scripts/build-zip.sh
+++ scripts/build-zip.sh
@@ -5,6 +5,6 @@ npm run build
 VERSION=$(node -p "require('./package.json').version")
 ZIP_NAME="prevent-duplicate-tabs-${VERSION}.zip"
 mkdir -p build
-rm -f "build/${ZIP_NAME}"
+rm -f build/prevent-duplicate-tabs-*.zip
 cd dist && zip -r "../build/${ZIP_NAME}" . && cd ..
 echo "Created build/${ZIP_NAME}"
